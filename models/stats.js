983ea1ed26fc4194e1827e8e3b9a32ce629a03bc
Justin L <justin@artah.net>
2020-12-17 13:07:39 -0500

    Added models

---
+++ models/stats.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class Stats {
+    constructor(db) {
+        this.db = db;
+    }
+
+    getTopScore() {
+        return 'Justin';
+    }
+
+    addScore(user, question, answer) {
+        let sql = `INSERT INTO stats(user, question, answer, datetime) 
+                   VALUES(?, ?, ?, CURRENT_TIMESTAMP)`;
+
+        this.db.run(sql, [user, question, answer], (err) => {
+            if (err) {
+                console.log("DB Error: "+err);
+            }
+        });
+    }
+
+    getTopScores(howMany, callback) {
+        let sql = `SELECT user, count(user) AS score 
+                   FROM stats
+                   GROUP BY user
+                   ORDER BY score DESC 
+                   LIMIT ` + howMany;
+
+        this.db.all(sql, [], (err, rows) => {
+            if (err) {
+                console.log("DB Error: "+err);
+            } else {
+                callback(rows);
+            }
+        });
+    }
+}
+
+module.exports = {
+    Stats: Stats
+}
